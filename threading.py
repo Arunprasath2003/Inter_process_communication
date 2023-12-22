817af7af65815fa31e90345b72d20f00705fe733
Arunprasath2003 <98107416+Arunprasath2003@users.noreply.github.com>
2023-12-22 13:31:52 +0530

    Threading technique

---
+++ threading.py
@@ -0,0 +1,110 @@
+import threading
+import time
+import random
+from queue import Queue
+
+class Server:
+    def __init__(self, name):
+        self.name = name
+        self.clients_attended_day = 0
+        self.clients_attended_month = 0
+        self.total_clients = 0
+        self.total_lost_clients = 0
+        self.average_rating = 0.0
+        self.current_client = None
+        self.client_queue = Queue()
+        self.lock = threading.Lock()
+
+    def serve_client(self, client):
+        # Simulate server-client interaction
+        time.sleep(random.uniform(0.1, 0.5))
+        # Obtain rating from client
+        rating = random.randint(1, 5)
+        with self.lock:
+            self.average_rating = (self.average_rating * self.clients_attended_day + rating) / (self.clients_attended_day + 1)
+            self.clients_attended_day += 1
+            self.clients_attended_month += 1
+            self.current_client = None
+
+    def start_serving(self):
+        while True:
+            if not self.client_queue.empty():
+                client = self.client_queue.get()
+                self.current_client = client.name
+                self.serve_client(client)
+            else:
+                time.sleep(1)
+
+    def run_server(self):
+        server_thread = threading.Thread(target=self.start_serving, daemon=True)
+        server_thread.start()
+
+class Client:
+    def __init__(self, name):
+        self.name = name
+        self.server_name = None
+        self.rating = None
+        self.chat_duration = 0  # Track the duration of the chat in seconds
+
+    def finish_chat(self, rating):
+        self.rating = rating
+        print(f"Client {self.name} has rated Server {self.server_name} with {rating} stars.")
+
+    def record_chat_duration(self, duration):
+        self.chat_duration = duration
+        print(f"Client {self.name} had a chat duration of {duration} seconds.")
+
+def simulate_server(server):#simulate the server
+    server.run_server()
+
+def simulate_client(client, server):
+    # Simulate client requesting server
+    start_time = time.time()
+    time.sleep(random.uniform(0.1, 0.5))
+    with server.lock:
+        if server.clients_attended_day >= 3:
+            print(f"Server {server.name} is busy. Client {client.name} is a lost client.")
+            server.total_lost_clients += 1
+        else:
+            server.client_queue.put(client)
+            client.server_name = server.name
+            server.total_clients += 1
+            print(f"Client {client.name} is being served by Server {server.name}.")
+    end_time = time.time()
+    client.record_chat_duration(end_time - start_time)
+
+
+if __name__ == "__main__":
+    # Create server instances
+    server_a = Server("Server A")
+    server_b = Server("Server B")
+
+    # Start server threads
+    threading.Thread(target=simulate_server, args=(server_a,), daemon=True).start()
+    threading.Thread(target=simulate_server, args=(server_b,), daemon=True).start()
+
+    # Simulate client-server communication
+    for i in range(1000):
+        client = Client(f"Client {i}")
+        if i % 2 == 0:
+            simulate_client(client, server_a)
+        else:
+            simulate_client(client, server_b)
+
+    # Allow time for threads to finish serving remaining clients
+    time.sleep(5)
+
+    # Print server statistics
+    print(f"\nServer A Statistics:")
+    print(f"Attended Clients (Day): {server_a.clients_attended_day}")
+    print(f"Attended Clients (Month): {server_a.clients_attended_month}")
+    print(f"Average Rating: {server_a.average_rating}")
+    print(f"Total Clients: {server_a.total_clients}")
+    print(f"Total Lost Clients: {server_a.total_lost_clients}")
+
+    print(f"\nServer B Statistics:")
+    print(f"Attended Clients (Day): {server_b.clients_attended_day}")
+    print(f"Attended Clients (Month): {server_b.clients_attended_month}")
+    print(f"Average Rating: {server_b.average_rating}")
+    print(f"Total Clients: {server_b.total_clients}")
+    print(f"Total Lost Clients: {server_b.total_lost_clients}")
